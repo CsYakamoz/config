3254e5a93fd8be455c16d92f59e98d81d343f11a
CsYakamoz <532609418@qq.com>
2022-04-19 01:08:50 +0800

    chore(wezterm): add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+local wezterm = require 'wezterm';
+
+return {
+	-- to see font config, use command: wezterm ls-fonts [--list-system]
+	-- for 13-inch
+	font_size = 17.6,
+	font = wezterm.font(
+		"SauceCodePro Nerd Font Mono",
+		{ weight = "DemiBold", stretch = "Normal", style = "Normal" }
+	),
+
+	color_scheme = "Builtin Solarized Light",
+
+	window_decorations = "RESIZE",
+	hide_tab_bar_if_only_one_tab = true,
+
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+}
