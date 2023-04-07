0a940c60877b1248e076102d702487a08217dab9
CsYakamoz <532609418@qq.com>
2023-04-08 02:24:05 +0800

    feat(wezterm): use font_with_fallback instead of font

--- wezterm/font.lua
+++ wezterm/font.lua
@@ -1,14 +1,21 @@
 local wezterm = require("wezterm")
 
+local PingFang = { family = "PingFang SC", weight = "Regular", stretch = "Normal", style = "Normal" }
+
 -- to see font config, use command: wezterm ls-fonts [--list-system]
 return {
-	SourceCodePro = wezterm.font(
-		"SauceCodePro Nerd Font Mono",
-		{ weight = "DemiBold", stretch = "Normal", style = "Normal" }
-	),
+	SourceCodePro = wezterm.font_with_fallback({
+		{ family = "SauceCodePro Nerd Font Mono", weight = "DemiBold", stretch = "Normal", style = "Normal" },
+		PingFang,
+	}),
 
-	IBMPlexMono = wezterm.font(
-		"BlexMono Nerd Font Mono",
-		{ weight = "DemiBold", stretch = "Normal", style = "Normal" }
-	),
+	IBMPlexMono = wezterm.font_with_fallback({
+		{ family = "BlexMono Nerd Font Mono", weight = "DemiBold", stretch = "Normal", style = "Normal" },
+		PingFang,
+	}),
+
+	Iosevka = wezterm.font_with_fallback({
+		{ family = "Iosevka Nerd Font", weight = "DemiBold", stretch = "Normal", style = "Normal" },
+		PingFang,
+	}),
 }
