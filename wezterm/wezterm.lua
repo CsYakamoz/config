6324b79d14297bc123402a253c889a8ec3ec91c9
CsYakamoz <532609418@qq.com>
2023-06-22 01:05:34 +0800

    feat(wezterm): use opacity & blur and toggle key

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,6 +4,8 @@ return {
 
 	window_decorations = "RESIZE",
 	hide_tab_bar_if_only_one_tab = true,
+	window_background_opacity = 0.90,
+	macos_window_background_blur = 20,
 
 	window_padding = {
 		left = 0,
