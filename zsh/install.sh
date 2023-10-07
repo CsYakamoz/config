1c6fcfe7ef12ff3e07cd9847746bd367eac8620e
CsYakamoz <532609418@qq.com>
2023-10-07 21:24:58 +0800

    refactor(zsh): rewrite install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-scriptDir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 if [[ -e "${HOME}/.oh-my-zsh" ]]; then
   echo "${HOME}/.oh-my-zsh already existed, ignored..."
@@ -11,22 +11,39 @@ else
   sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-if [[ -e "${HOME}/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]]; then
-  echo "${HOME}/.oh-my-zsh/custom/plugins/zsh-autosuggestions already existed, ignored..."
-else
-  echo "installing zsh-autosuggestions..."
-  git clone https://github.com/zsh-users/zsh-autosuggestions \
-    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-fi
+function _zsh_install_plugin() {
+  PLUGIN_NAME="$(basename "$1")"
+  CUSTOM_PLUGIN="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins"
 
-if [[ -e "${HOME}/.oh-my-zsh/custom/plugins/fast-syntax-highlighting" ]]; then
-  echo "${HOME}/.oh-my-zsh/custom/plugins/fast-syntax-highlighting already existed, ignored..."
-else
-  echo "installing fast-syntax-highlighting..."
-  git clone https://github.com/zdharma/fast-syntax-highlighting.git \
-    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting"
-fi
+  if [[ -e "${CUSTOM_PLUGIN}/${PLUGIN_NAME}" ]]; then
+    echo "${CUSTOM_PLUGIN}/${PLUGIN_NAME} already existed, ignored..."
+  else
+    echo "installing ${PLUGIN_NAME}..."
+    git clone "$1" "${CUSTOM_PLUGIN}/${PLUGIN_NAME}"
+  fi
+}
 
-/bin/cp -i "${scriptDir}/zshrc" "${HOME}/.zshrc"
+function _zsh_backup() {
+  target="$1"
+  name=$(basename "$1")
+  backup="/tmp/$(date +%Y%m%d_%H%M%S)_${name}.bak"
+
+  if [[ ! -e "${target}" ]]; then
+    return 0
+  fi
+
+  echo "backup ${target} to ${backup}"
+  /bin/cp -i "${target}" "${backup}"
+}
+
+_zsh_install_plugin "https://github.com/zsh-users/zsh-autosuggestions"
+_zsh_install_plugin "https://github.com/z-shell/F-Sy-H.git"
+_zsh_install_plugin "https://github.com/hlissner/zsh-autopair.git"
+
+_zsh_backup "${HOME}/.zshrc"
+_zsh_backup "${HOME}/.p10k.zsh"
+
+/bin/cp -i "${script_dir}/zshrc" "${HOME}/.zshrc"
+/bin/cp -i "${script_dir}/p10k.zsh" "${HOME}/.p10k.zsh"
 
 echo "zshrc/install.sh finish..."
